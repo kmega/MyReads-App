39262dfea0fcac18da51b377a889f2a3f4c4dd6a
Charles <charles.sz@protonmail.com>
2020-02-11 17:53:06 +0100

    Created gh-pages for site hosting.

---
+++ my-project/build/precache-manifest.75edb81901da355826de1d2b2d8a7cac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88516ff4f61966e71a50c1218f3fce9",
+    "url": "/myreads-app/index.html"
+  },
+  {
+    "revision": "69392464e9d7971b62e5",
+    "url": "/myreads-app/static/css/main.04aa7a12.chunk.css"
+  },
+  {
+    "revision": "5b735cdad03843cfd5de",
+    "url": "/myreads-app/static/js/2.168016f3.chunk.js"
+  },
+  {
+    "revision": "8e3420f354ad8e6491c4bf161072f1dc",
+    "url": "/myreads-app/static/js/2.168016f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69392464e9d7971b62e5",
+    "url": "/myreads-app/static/js/main.b181e669.chunk.js"
+  },
+  {
+    "revision": "643101b6eedc0183c9f3",
+    "url": "/myreads-app/static/js/runtime-main.3472a9ca.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads-app/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads-app/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads-app/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
