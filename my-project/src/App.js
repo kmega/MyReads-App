8aedff701cd9b8ba7e6548e58c3f1888a2cc7ffb
Charles <charles.sz@protonmail.com>
2020-02-12 18:04:16 +0100

    Freshen up the code.

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -8,122 +8,114 @@ import BookShelf from "./BookShelf";
 import SearchOption from "./SearchOption";
 // State with all books.
 class BooksApp extends React.Component {
-    state = {
-        books: []
-    }
-    // Render books that are currently at shelves.
-    componentDidMount() {
-        BooksAPI.getAll().then((books) => {
-            this.setState({
-                books
-            });
-        })
-    }
-    // A function that changes shelf based on initial value.
-    changeShelf = (targetBook, targetShelf) => {
-        BooksAPI.update(targetBook, targetShelf).then(() => {
-            targetBook.shelf = targetShelf
-            this.setState({
-                // Place the book at respective shelf.
-                books: this.state.books.filter((t) => t.id !== targetBook.id).concat(targetBook)
-            })
-        })
-    }
-    render() {
-        // Currently available shelves and their names.
-        const bookStatus = [{
-                status: "currentlyReading",
-                statusName: "Currently Reading"
-            },
-            {
-                status: "wantToRead",
-                statusName: "Want to Read"
-            },
-            {
-                status: "read",
-                statusName: "Read"
-            }
-        ];
-        // Here application starts.
-        return ( <
-            div className = "app" >
-            <
-            div className = "open-search" >
-            <
-            a href = "/search" > Add a book < /a>
-						< /div> { /* Creating main route for rendering purposes. */ }
-            <Router basename = "/my-project">
-            <Route exact path = "/"
-            render = {
-                () => ( <
-                    div className = "list-books" >
-                    <
-                    div className = "list-books-title" >
-                    <
-                    h1 > MyReads < /h1> <
-                    /div> <
-                    div className = "list-books-content" > { /* Render books that initially are at begining. */ } <
-                    div > {
-                        bookStatus.map((shelf, iteration) => {
-                            const books = this.state.books.filter(book => book.shelf === shelf.status)
-                            return ( <
-                                BookShelf key = {
-                                    shelf.status
-                                }
-                                books = {
-                                    books
-                                }
-                                shelf = {
-                                    shelf.statusName
-                                }
-                                changeShelf = {
-                                    this.changeShelf
-                                }
-                                />
-                            );
-                        })
-                    } < /div> <
-                    /div> <
-                    /div>
-                )
-            }/>
-            </Router>
-            { /* Creating main route for rendering purposes. */ }
-            <Router basename = "/my-project">
-            <Route exact path = "/search"
-            render = {
-                () => ( <
-                    div className = "search-books" >
-                    <
-                    div className = "search-books-bar" >
-                    <
-                    a className = "close-search"
-                    href = "/" > Close < /a> <
-                    div className = "search-books-input-wrapper" >
-                    <
-                    SearchOption type = "text"
-                    placeholder = "Search by title or author"
-										books = {
-											this.state.books
-										}
-                    changeShelf = {
-                        this.changeShelf
-                    }
-                    /> <
-                    /div> <
-                    /div> <
-                    div className = "search-books-results" >
-                    <
-                    ol className = "books-grid" > < /ol> <
-                    /div> <
-                    /div>
-                )
-            }
-            />
-            </Router>
+  state = {
+    books: []
+  };
+  // Render books that are currently at shelves.
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({
+        books
+      });
+    });
+  }
+  // A function that changes shelf based on initial value.
+  changeShelf = (targetBook, targetShelf) => {
+    BooksAPI.update(targetBook, targetShelf).then(() => {
+      targetBook.shelf = targetShelf;
+      this.setState({
+        // Place the book at respective shelf.
+        books: this.state.books
+          .filter(t => t.id !== targetBook.id)
+          .concat(targetBook)
+      });
+    });
+  };
+  render() {
+    // Currently available shelves and their names.
+    const bookStatus = [
+      {
+        status: "currentlyReading",
+        statusName: "Currently Reading"
+      },
+      {
+        status: "wantToRead",
+        statusName: "Want to Read"
+      },
+      {
+        status: "read",
+        statusName: "Read"
+      }
+    ];
+    // Here application starts.
+    return (
+      <div className="app">
+        <div className="open-search">
+          <a href="/search"> Add a book </a>
+        </div>
+        {/* Creating main route for rendering purposes. */}
+        <Router basename="/my-project">
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1> MyReads </h1>
+                </div>
+                <div className="list-books-content">
+
+                  {/* Render books that initially are at begining. */}
+                  <div>
+
+                    {bookStatus.map((shelf, iteration) => {
+                      const books = this.state.books.filter(
+                        book => book.shelf === shelf.status
+                      );
+                      return (
+                        <BookShelf
+                          key={shelf.status}
+                          books={books}
+                          shelf={shelf.statusName}
+                          changeShelf={this.changeShelf}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            )}
+          />
+        </Router>
+        {/* Creating search route for rendering purposes. */}
+        <Route
+          exact
+          path="/search"
+          render={() => (
+            <div className="search-books">
+              <div className="search-books-bar">
+                <a className="close-search" href="/">
+
+                  Close
+                </a>
+                <div className="search-books-input-wrapper">
+                  <SearchOption
+                    type="text"
+                    placeholder="Search by title or author"
+                    books={this.state.books}
+                    changeShelf={this.changeShelf}
+                  />
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid"> </ol>
+              </div>
             </div>
-        )
-    }
+          )}
+        />
+      </div>
+    );
+  }
 }
 
-export default BooksApp
+export default BooksApp;
--- my-project/src/SearchOption.js
+++ my-project/src/SearchOption.js
@@ -1,88 +1,71 @@
-import React, {
-    Component
-} from "react";
+import React, { Component } from "react";
 // Imported BooksAPI for search option to work.
 import * as BooksAPI from "./BooksAPI";
 import Book from "./Book";
 // Component responsible for searching books.
 class SearchOption extends Component {
-    // State with user input.
-    state = {
-        query: "",
-        searchResults: [],
-        showNothing: false
-    }
-    // Search method for searching terms.
-    search = (event) => {
-        const query = event.target.value.trim()
-        this.setState({
-            query: query
-        })
-        // If user typed then start the search.
-        if (query) {
-            BooksAPI.search(query).then(notInShelves => {
-                // If no error occurred proceed.
-                if (!notInShelves.error) {
-                    // Compare shelves in order to display correct books.
-                    notInShelves.forEach((newBook) => {
-                        this.props.books.forEach((onShelf) => {
-                            if (onShelf.id === newBook.id) {
-                                newBook.shelf = onShelf.shelf
-                            } else {
-                                newBook.shelf = "none"
-                            }
-                        })
-                    })
-                    // After comparing show all books
-                    this.setState({
-                        searchResults: notInShelves,
-                        showNothing: false
-                    })
-                }
-                // If search terms aren't met or books can't be found then show empt page.
-                else {
-                    this.setState({
-                        searchResults: [],
-                        showNothing: true
-                    })
-                }
-            })
+  // State with user input.
+  state = {
+    query: "",
+    searchResults: [],
+    showNothing: false
+  };
+  // Search method for searching terms.
+  search = event => {
+    const query = event.target.value.trim();
+    this.setState({
+      query: query
+    });
+    // If user typed then start the search.
+    if (query) {
+      BooksAPI.search(query).then(notInShelves => {
+        // If no error occurred proceed.
+        if (!notInShelves.error) {
+          // Compare shelves in order to display correct books.
+          notInShelves.forEach(newBook => {
+            this.props.books.forEach(onShelf => {
+              if (onShelf.id === newBook.id) {
+                newBook.shelf = onShelf.shelf;
+              } else {
+                newBook.shelf = "none";
+              }
+            });
+          });
+          // After comparing show all books
+          this.setState({
+            searchResults: notInShelves,
+            showNothing: false
+          });
         }
+        // If search terms aren't met or books can't be found then show empty page.
+        else {
+          this.setState({
+            searchResults: [],
+            showNothing: true
+          });
+        }
+      });
     }
-    render() {
-        const {
-            changeShelf
-        } = this.props;
-        return ( <
-            section className = "app" >
-            <
-            input type = "text"
-            placeholder = "Search by Title or Author"
-            value = {
-                this.state.query
-            }
-            onChange = {
-                this.search
-            }
-            /> <
-            div className = "books-search-grid" > {
-                this.state.searchResults.map(book => ( <
-                    Book key = {
-                        book.id
-                    }
-                    book = {
-                        book
-                    }
-                    changeShelf = {
-                        changeShelf
-                    }
-                    />
-                ))
-            } <
-            /div> < /
-            section >
-        )
-    }
+  };
+  render() {
+    const { changeShelf } = this.props;
+    return (
+      <section className="app">
+        <input
+          type="text"
+          placeholder="Search by Title or Author"
+          value={this.state.query}
+          onChange={this.search}
+        />
+        <div className="books-search-grid">
+
+          {this.state.searchResults.map(book => (
+            <Book key={book.id} book={book} changeShelf={changeShelf} />
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default SearchOption;
