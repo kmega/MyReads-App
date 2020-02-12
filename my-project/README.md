d55f5101bb148d1b21db39c2a5f6c92e00d89e14
Charles <charles.sz@protonmail.com>
2020-02-12 13:32:46 +0100

    Using HashRouter as Router for deployment.

---
+++ my-project/README.md
@@ -0,0 +1,13 @@
+﻿MyReads Project by Charles Szelazek
+
+How to install?
+
+In git bash terminal you have to run 'npm start' in order to run the app. App will be running on 'localhost:3000'.
+
+How to use it?
+
+There are three bookshelves which are representing state of book you're reading, will be reading or already read. In order to change shelf you have to click on the icon below book. If you want to search for other titles then click 'plus icon' in the bottom left corner and then search using 'SEARCH_TERMS' provided in this project by Udacity.
+
+What can you do with it?
+
+As long as you'll follow Udacity’s definition of plagiarism you can use this project however you want.
---
+++ my-project/src/Book.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import coverNotAvailable from './icons/cover-no-available-image.png';
+import PropTypes from 'prop-types';
+// Component responsible for rendering books.
+class Book extends React.Component {
+    // PropTypes for proper data definition.
+    static propTypes = {
+        book: PropTypes.object.isRequired
+    }
+    render() {
+        const {
+            book,
+            changeShelf
+        } = this.props
+        // If image is not available then use 'no cover image'.
+        const coverSetup = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : coverNotAvailable
+        return (
+            // Creating books.
+            <
+            li >
+            <
+            div className = 'book' >
+            <
+            div className = 'book-top' >
+            <
+            div className = 'book-cover' >
+            <
+            img src = {
+                coverSetup
+            }
+            alt = {
+                `${book.title} cover`
+            } ></img> <
+            /div> <
+            div className = 'book-shelf-changer' > { /* Creating options for books to move. */ } <
+            select value = {
+                book.shelf
+            }
+            onChange = {
+                (event) => changeShelf(book, event.target.value)
+            } >
+            <
+            option value = 'move'
+            disabled > Move to... < /option> <
+            option value = 'none' > None < /option> <
+            option value = 'currentlyReading' > Currently Reading < /option> <
+            option value = 'wantToRead' > Want to Read < /option> <
+            option value = 'read' > Read < /option> <
+            /select> <
+            /div>
+						< /div> { /* Writing title and authors. */ } <
+            h3 className = 'book-title' > {
+                book.title
+            } < /h3> <
+            h4 className = 'book-authors' > {
+                book.authors
+            } < /h4> <
+            /div> <
+            /li>
+        )
+    }
+}
+
+export default Book
