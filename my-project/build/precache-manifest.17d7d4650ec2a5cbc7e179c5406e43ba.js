de9b311d2ecd415f3a99aeeac283198419a00330
Charles <charles.sz@protonmail.com>
2020-02-12 12:55:07 +0100

    Should host on GitHub Pages.

---
+++ my-project/build/precache-manifest.17d7d4650ec2a5cbc7e179c5406e43ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3220bbdc7175f6b02b79feb1d51d77",
+    "url": "/kmega/MyReads-App/index.html"
+  },
+  {
+    "revision": "f61ce2e5ae6e3ef459de",
+    "url": "/kmega/MyReads-App/static/css/main.23814e9c.chunk.css"
+  },
+  {
+    "revision": "5b735cdad03843cfd5de",
+    "url": "/kmega/MyReads-App/static/js/2.168016f3.chunk.js"
+  },
+  {
+    "revision": "8e3420f354ad8e6491c4bf161072f1dc",
+    "url": "/kmega/MyReads-App/static/js/2.168016f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61ce2e5ae6e3ef459de",
+    "url": "/kmega/MyReads-App/static/js/main.b181e669.chunk.js"
+  },
+  {
+    "revision": "864460d61b7953691050",
+    "url": "/kmega/MyReads-App/static/js/runtime-main.1d881c3d.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/kmega/MyReads-App/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/kmega/MyReads-App/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/kmega/MyReads-App/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
